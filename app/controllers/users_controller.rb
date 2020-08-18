afd8ab3ec3c98c0b5fd614d09265545279b7f71e
Philip Tynan <tynan_philip@me.com>
2020-08-18 14:01:43 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
+    def show
+    end
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :name, :password)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+    after_initialize :ensure_session_token
+    validates :email, :name, :password_digest, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def check_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user
+            user.check_password?(password) ? user : nil
+        else
+            nil
+        end
+    end
+
+end
