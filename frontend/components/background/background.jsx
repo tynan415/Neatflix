6f1a87c4ae16b6e1065d500cef8f72bc265874ea
Philip Tynan <tynan_philip@me.com>
2020-08-22 10:55:56 -0700

    css chaos

--- frontend/components/background/background.jsx
+++ frontend/components/background/background.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
 class SplashPage extends React.Component {
+
+
     render() {
 
         let getStarted;
-        if (this.props.location.pathname !== "/login"){
-            getStarted = <input id="getStarted" type="text"/> 
+        if (this.props.location.pathname !== "/login") {
+            getStarted = <div>
+                <input 
+                id="getStarted"
+                type="text"
+                placeholder="Email adress"
+                /> 
+                <button id="getBut">GET STARTED ></button>
+            </div>
         }
 
+        let welcome;
+        if (this.props.location.pathname !== "/login") {
+            welcome = <div className="welcome">
+                <h1 id="welcomeH1">Unlimited movies, TV shows, and more.</h1>
+                <h3 id="welcomeH3">Watch anywhere. Cancel anytime.</h3>
+                <p id="welcomeP">Ready to watch? Enter your email to create a membership, or try our</p>
+                <input type="submit" value="demo Login" id="demoBut" />
+            </div>
+        }
 
         return(
             <div id="gradient">
+
                 <div id="backGround"/>
+                {welcome}
                 {getStarted}
             </div>
         )
