6b312ebed1c1af2fae673e0cfb6886c0d141af79
tynan415 <tynan_philip@me.com>
2023-01-09 22:53:47 -0800

    edits

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false # i added this -pt 1/9/23
 
 module Neatflix
   class Application < Rails::Application
