08f89cd1878892cf577dfebab7eaa420ea13b7ee
tynan415 <tynan_philip@me.com>
2023-01-09 20:50:24 -0800

    heroku

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,10 @@ Bundler.require(*Rails.groups)
 module Neatflix
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false # i added this -pt 1/9/23
+
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false # i added this -pt 1/9/23
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
