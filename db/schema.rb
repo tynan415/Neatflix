9ea7e5479eaea3fa1ced4383e09134e1ae243463
tynan415 <tynan_philip@me.com>
2023-01-09 23:38:16 -0800

    local errors

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_202450) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_01_27_202450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_202450) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -32,23 +31,23 @@ ActiveRecord::Schema.define(version: 2021_01_27_202450) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "genre_types", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_genre_types_on_name"
   end
 
   create_table "my_lists", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "video_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id", "video_id"], name: "index_my_lists_on_user_id_and_video_id", unique: true
     t.index ["user_id"], name: "index_my_lists_on_user_id"
     t.index ["video_id"], name: "index_my_lists_on_video_id"
@@ -58,8 +57,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_202450) do
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["session_token"], name: "index_users_on_session_token"
   end
@@ -67,8 +66,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_202450) do
   create_table "video_genres", force: :cascade do |t|
     t.integer "video_id", null: false
     t.integer "genre_type_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["genre_type_id"], name: "index_video_genres_on_genre_type_id"
     t.index ["video_id"], name: "index_video_genres_on_video_id"
   end
@@ -77,8 +76,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_202450) do
     t.string "title", null: false
     t.boolean "liked"
     t.string "description", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
+          options: {
             presets: ['@babel/env', '@babel/react'],
             "plugins": [
               ["@babel/plugin-proposal-class-properties", { "loose": true }]
